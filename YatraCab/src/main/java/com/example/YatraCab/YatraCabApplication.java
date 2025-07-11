2b84a2d08ce3ee58ef6272999b3b70b35f0d2336
surya <suryakant231216@gmail.com>
2025-07-11 14:47:42 +0530

    c

---
+++ YatraCab/src/main/java/com/example/YatraCab/YatraCabApplication.java
@@ -0,0 +1,13 @@
+package com.example.YatraCab;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class YatraCabApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(YatraCabApplication.class, args);
+	}
+
+}
---
+++ YatraCab/src/test/java/com/example/YatraCab/YatraCabApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.YatraCab;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class YatraCabApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
